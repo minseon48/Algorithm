9fa1b51f307b4a8c8c33d5f51c0accc0985f263a
minseon48 <97889824+minseon48@users.noreply.github.com>
2023-11-28 12:27:00 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1532 ms, Memory: 144268 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,19 +1,20 @@
 import java.io.*;
-import java.util.TreeSet;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        TreeSet<Integer> treeSet = new TreeSet<>();
+        List<Integer> list = new ArrayList<>();
         int n = Integer.parseInt(br.readLine());
 
         for(int i = 0; i<n;i++){
             String s = br.readLine();
-            treeSet.add(Integer.parseInt(s));
+            list.add(Integer.parseInt(s));
         }
+        Collections.sort(list);
 
-        for (int i : treeSet) {
+        for (int i : list) {
             bw.write(String.valueOf(i));
             bw.newLine();
 
