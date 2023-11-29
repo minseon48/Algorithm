0054934dbfd6e982261d4c9685564e4106a51c8e
minseon48 <97889824+minseon48@users.noreply.github.com>
2023-11-29 11:17:15 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -4,18 +4,16 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> list = new ArrayList<>();
         int n = Integer.parseInt(br.readLine());
         int[] p = new int[n];
         int minSum = 0;
         String[] s = br.readLine().split(" ");
-        for(String x : s){
-            list.add(Integer.parseInt(x));
-        }
-        Collections.sort(list);
+
         for(int i = 0; i<n;i++){
-           p[i] = list.get(i);
+           p[i] = Integer.parseInt(s[i]);
         }
+        Arrays.sort(p);
+
         for(int i = 1;i<n;i++){
             p[i] = p[i-1]+p[i];
 
