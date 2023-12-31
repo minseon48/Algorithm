4e2748742f722d8903748abe3dc7680a93f40d8b
minseon48 <97889824+minseon48@users.noreply.github.com>
2023-12-31 16:48:46 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -2,12 +2,13 @@ import java.io.*;
 
 
 class Main {
+    static int fibo[];
 
     public int DFS(int n){
-        if(n==0) return 0;
-        else if(n==1) return 1;
-        else if(n==2) return 1;
-        else return DFS(n-2)+DFS(n-1);
+        if(n==0) return fibo[n]= 0;
+        else if(n==1) return fibo[n]= 1;
+        else if(n==2) return fibo[n] = 1;
+        else return fibo[n] = DFS(n-2)+DFS(n-1);
 
     }
 
@@ -16,8 +17,10 @@ class Main {
         Main m = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        fibo = new int[n+1];
 
-        System.out.println(m.DFS(n));
+        m.DFS(n);
+        System.out.println(fibo[n]);
 
 
     }
