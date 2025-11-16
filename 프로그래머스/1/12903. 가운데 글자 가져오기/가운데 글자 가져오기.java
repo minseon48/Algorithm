04ed2a68e7fae1ef193ccb11a43de1dc2c7ee850
minseon48 <97889824+minseon48@users.noreply.github.com>
2025-11-17 01:55:12 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 13.57 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+
+        for(int i = 0; i<len; i++){
+            if(len % 2 == 0){
+                answer += s.charAt(len/2 - 1);
+                answer += s.charAt(len/2);
+                break;
+            }else{
+                answer += s.charAt(len/2);
+                break;
+            }
+        }
+        return answer;
+
+    }
+}
